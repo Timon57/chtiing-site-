f4940fd8fcd1ba710aa6567b867fe9c360b89e57
Timon57 <timobasnet@gmail.com>
2022-12-11 19:17:32 +0545

    profile and message model created

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg',upload_to='images')
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Message(models.Model):
+    body = models.TextField()
+    owner = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:10]
