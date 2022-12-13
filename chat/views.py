da4e852efbaa1708e5bd37b9e394c85640ebf3e8
Timon57 <timobasnet@gmail.com>
2022-12-13 21:13:18 +0545

    created login and register

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
+from .models import Profile
+from .forms import SignInForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'chat/home.html')
+
+def registerPage(request):
+    form = SignInForm
+    if request.method == "POST":
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {
+        'form':form
+    }
+    return render(request,'chat/register.html',context)
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        passsword = request.POST['password']
+        user = authenticate(request,username=username,password=passsword)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    context = {
+    }
+    return render(request,'chat/login.html',context)
+    
+def logoutPage(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('home')
+    return render(request,'chat/logout.html')
+        
